e30728323366aff8666dbf4afc84aaa4e8735b48
SMSC-PMacmini <smscmacmini2@macmini2.local>
2018-12-26 10:53:19 +0530

    Name added

--- iOSAppTest/ViewController.swift
+++ iOSAppTest/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // PhanidhraChowdary
+        print("PhanidhraChowdary")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
