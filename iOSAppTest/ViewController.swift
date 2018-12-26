4da0adab4299ce82b06462dfdc9c62883570a5cb
SMSC-PMacmini <smscmacmini2@macmini2.local>
2018-12-26 11:27:45 +0530

    space added in Vc

--- iOSAppTest/ViewController.swift
+++ iOSAppTest/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         // PhanidhraChowdary
+        
+        
         print("PhanidhraChowdary")
         // Do any additional setup after loading the view, typically from a nib.
     }
